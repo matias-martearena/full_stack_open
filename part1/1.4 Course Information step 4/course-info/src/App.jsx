508538208f49072ad396555bc8e68a4c68dd6f39
Matías Martearena <maticorvooficial@gmail.com>
2024-04-29 16:59:22 -0300

    1.4 Course Info solved

---
+++ part1/1.4 Course Information step 4/course-info/src/App.jsx
@@ -0,0 +1,56 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => (
+        <Part key={part.name} part={part}/>
+      ))}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const totalExercises = props.parts.reduce((acc, part) => acc + part.exercises, 0)
+  return (
+    <p>Number of exercises {totalExercises}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts}/>
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
