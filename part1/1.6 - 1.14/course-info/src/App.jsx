06f6df59d435b0f3f19a7f738ce0ac93a44f9e36
Matías Martearena <maticorvooficial@gmail.com>
2024-04-30 20:17:50 -0300

    1.7 solved

--- part1/1.6 - 1.14/course-info/src/App.jsx
+++ part1/1.6 - 1.14/course-info/src/App.jsx
@@ -10,10 +10,28 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+    setAverage(average + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+    setAverage(average - 1)
+  }
+
+  const averageResult = total > 0 ? (average / total) : 0
+  const goodPercentage = total > 0 ? ((good / total) * 100) : 0
 
   return (
     <div>
@@ -25,6 +43,9 @@ const App = () => {
       <Statistic text={'Good'} value={good}/>
       <Statistic text={'Neutral'} value={neutral}/>
       <Statistic text={'Bad'} value={bad}/>
+      <Statistic text={'All'} value={total}/>
+      <Statistic text={'Average'} value={averageResult}/>
+      <Statistic text={'Positive'} value={goodPercentage}/>
     </div>
   )
 }
