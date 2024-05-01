fc21c69a1078c942e84fc3332db70e018211da05
Matías Martearena <maticorvooficial@gmail.com>
2024-04-30 22:33:37 -0300

    1.12 solved

--- part1/1.6 - 1.14/course-info/src/App.jsx
+++ part1/1.6 - 1.14/course-info/src/App.jsx
@@ -1,5 +1,14 @@
 import { useState } from 'react'
 
+const Anecdote = ({ onRandomAnecdote, anecdote }) => {
+  return (
+    <div>
+      <p>{anecdote}</p>
+      <button onClick={onRandomAnecdote}>Next anecdote</button>
+    </div>
+  )
+}
+
 const Text = ({ text }) => <h2>{text}</h2>
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
@@ -38,6 +47,23 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [total, setTotal] = useState(0)
   const [average, setAverage] = useState(0)
+  const [selected, setSelected] = useState(0)
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const getRandomAnecdote = () => {
+    const randomNumber = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomNumber)
+  }
 
   const handleGoodClick = () => {
     setGood(good + 1)
@@ -61,6 +87,7 @@ const App = () => {
 
   return (
     <div>
+      <Anecdote anecdote={anecdotes[selected]} onRandomAnecdote={getRandomAnecdote}/>
       <Text text={'Give feedback'} />
       <Button handleClick={handleGoodClick} text={'good'} />
       <Button handleClick={handleNeutralClick} text={'neutral'} />
