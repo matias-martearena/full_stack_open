b80b414fe01b483e39a112bc0b9458d08a0fc50e
Matías Martearena <maticorvooficial@gmail.com>
2024-05-06 19:56:11 -0300

    2.11 The Phonebook step 6

---
+++ part2/2.11 The Phonebook/src/App.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [findName, setFindName] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addPerson = e => {
+    e.preventDefault()
+
+    if (persons.find(person => person.name === newName)) return alert(`${newName} is already added to phonebook`)
+
+    const personObject = {
+      id: persons.length + 1,
+      name: newName,
+      number: newNumber
+    }
+
+    setPersons([...persons, personObject])
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleFindName = e => setFindName(e.target.value)
+  const handleNameChange = e => setNewName(e.target.value)
+  const handleNumberChange = e => setNewNumber(e.target.value)
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(findName.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter name={findName} onFindName={handleFindName} />
+      <PersonForm
+        onSubmitForm={addPerson}
+        name={newName}
+        onChageName={handleNameChange}
+        number={newNumber}
+        onChangeNumber={handleNumberChange}
+      />
+      <Person filter={filteredPersons}/>
+    </div>
+  )
+}
+
+export default App
