6c8d8f5ff8cb4360e83ee5d45b5cae9e5f369be0
Matías Martearena <maticorvooficial@gmail.com>
2024-05-30 22:42:24 -0300

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/part4-a/utils/list_helper.js
+++ part4/part4-a/utils/list_helper.js
@@ -7,3 +7,22 @@ export const totalLikes = array => {
     ? 0
     : array.reduce((acc, item) => acc + item.likes, 0)
 }
+
+export const favoriteBlog = blogs => {
+  if (!Array.isArray(blogs)) throw new Error('Input must be an array of blog objects')
+  if (blogs.length === 0) return 0
+
+  let favoriteBlog = null
+  let maxLikes = 0
+
+  for (const blog of blogs) {
+    const currentLikes = blog.likes ?? 0
+
+    if (currentLikes > maxLikes) {
+      favoriteBlog = blog
+      maxLikes = currentLikes
+    }
+  }
+
+  return favoriteBlog
+}
