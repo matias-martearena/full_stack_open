62bfbd645cf2fde4327c6c0c649b99b4c54f80e4
Matías Martearena <maticorvooficial@gmail.com>
2024-05-30 22:22:05 -0300

    4.4: Helper Functions and Unit Tests, step 2

--- part4/part4-a/utils/list_helper.js
+++ part4/part4-a/utils/list_helper.js
@@ -1,3 +1,9 @@
 export const dummy = array => {
   return 1
 }
+
+export const totalLikes = array => {
+  return array.length === 0
+    ? 0
+    : array.reduce((acc, item) => acc + item.likes, 0)
+}
